4cb36b983cb38dd6003eddd36eb2a1b3006cf177
alexandr-katane <akatane18.19@gmail.com>
2022-05-05 18:25:17 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,429 @@
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
+
+const iconMenu = document.querySelector('.icon-menu');
+const menuBody = document.querySelector('.menu__body');
+
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
+
+
+
+function ibg() {
+
+    let ibg = document.querySelectorAll("._ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+            if (ibg[i].classList.contains('home__back')) {
+                ibg[i].style.backgroundImage = 'linear-gradient(0deg, rgba(15, 15, 15, 0.5), rgba(15, 15, 15, 0.5)),url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+            }
+            if (ibg[i].classList.contains('season__back') || ibg[i].classList.contains('collection__back')) {
+                ibg[i].style.backgroundImage = 'linear-gradient(0deg, rgba(15, 15, 15, 0.5), rgba(15, 15, 15, 0.5)),url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+            }
+            if (ibg[i].classList.contains('footer__back')) {
+                ibg[i].style.backgroundImage = 'linear-gradient(0deg, rgba(15, 15, 15, 0.85), rgba(15, 15, 15, 0.85)),url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+            }
+        }
+    }
+}
+
+ibg();
+
+;
+// Dynamic Adapt v.1
+// HTML data-da="where(uniq class name),when(breakpoint),position(digi)"
+// e.x. data-da=".item,992,2"
+// Andrikanych Yevhen 2020
+// https://www.youtube.com/c/freelancerlifestyle
+
+"use strict";
+
+function DynamicAdapt(type) {
+	this.type = type;
+}
+
+DynamicAdapt.prototype.init = function () {
+	const _this = this;
+	// массив объектов
+	this.оbjects = [];
+	this.daClassname = "_dynamic_adapt_";
+	// массив DOM-элементов
+	this.nodes = document.querySelectorAll("[data-da]");
+
+	// наполнение оbjects объктами
+	for (let i = 0; i < this.nodes.length; i++) {
+		const node = this.nodes[i];
+		const data = node.dataset.da.trim();
+		const dataArray = data.split(",");
+		const оbject = {};
+		оbject.element = node;
+		оbject.parent = node.parentNode;
+		оbject.destination = document.querySelector(dataArray[0].trim());
+		оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+		оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+		оbject.index = this.indexInParent(оbject.parent, оbject.element);
+		this.оbjects.push(оbject);
+	}
+
+	this.arraySort(this.оbjects);
+
+	// массив уникальных медиа-запросов
+	this.mediaQueries = Array.prototype.map.call(this.оbjects, function (item) {
+		return '(' + this.type + "-width: " + item.breakpoint + "px)," + item.breakpoint;
+	}, this);
+	this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, function (item, index, self) {
+		return Array.prototype.indexOf.call(self, item) === index;
+	});
+
+	// навешивание слушателя на медиа-запрос
+	// и вызов обработчика при первом запуске
+	for (let i = 0; i < this.mediaQueries.length; i++) {
+		const media = this.mediaQueries[i];
+		const mediaSplit = String.prototype.split.call(media, ',');
+		const matchMedia = window.matchMedia(mediaSplit[0]);
+		const mediaBreakpoint = mediaSplit[1];
+
+		// массив объектов с подходящим брейкпоинтом
+		const оbjectsFilter = Array.prototype.filter.call(this.оbjects, function (item) {
+			return item.breakpoint === mediaBreakpoint;
+		});
+		matchMedia.addListener(function () {
+			_this.mediaHandler(matchMedia, оbjectsFilter);
+		});
+		this.mediaHandler(matchMedia, оbjectsFilter);
+	}
+};
+
+DynamicAdapt.prototype.mediaHandler = function (matchMedia, оbjects) {
+	if (matchMedia.matches) {
+		for (let i = 0; i < оbjects.length; i++) {
+			const оbject = оbjects[i];
+			оbject.index = this.indexInParent(оbject.parent, оbject.element);
+			this.moveTo(оbject.place, оbject.element, оbject.destination);
+		}
+	} else {
+		for (let i = 0; i < оbjects.length; i++) {
+			const оbject = оbjects[i];
+			if (оbject.element.classList.contains(this.daClassname)) {
+				this.moveBack(оbject.parent, оbject.element, оbject.index);
+			}
+		}
+	}
+};
+
+// Функция перемещения
+DynamicAdapt.prototype.moveTo = function (place, element, destination) {
+	element.classList.add(this.daClassname);
+	if (place === 'last' || place >= destination.children.length) {
+		destination.insertAdjacentElement('beforeend', element);
+		return;
+	}
+	if (place === 'first') {
+		destination.insertAdjacentElement('afterbegin', element);
+		return;
+	}
+	destination.children[place].insertAdjacentElement('beforebegin', element);
+}
+
+// Функция возврата
+DynamicAdapt.prototype.moveBack = function (parent, element, index) {
+	element.classList.remove(this.daClassname);
+	if (parent.children[index] !== undefined) {
+		parent.children[index].insertAdjacentElement('beforebegin', element);
+	} else {
+		parent.insertAdjacentElement('beforeend', element);
+	}
+}
+
+// Функция получения индекса внутри родителя
+DynamicAdapt.prototype.indexInParent = function (parent, element) {
+	const array = Array.prototype.slice.call(parent.children);
+	return Array.prototype.indexOf.call(array, element);
+};
+
+// Функция сортировки массива по breakpoint и place 
+// по возрастанию для this.type = min
+// по убыванию для this.type = max
+DynamicAdapt.prototype.arraySort = function (arr) {
+	if (this.type === "min") {
+		Array.prototype.sort.call(arr, function (a, b) {
+			if (a.breakpoint === b.breakpoint) {
+				if (a.place === b.place) {
+					return 0;
+				}
+
+				if (a.place === "first" || b.place === "last") {
+					return -1;
+				}
+
+				if (a.place === "last" || b.place === "first") {
+					return 1;
+				}
+
+				return a.place - b.place;
+			}
+
+			return a.breakpoint - b.breakpoint;
+		});
+	} else {
+		Array.prototype.sort.call(arr, function (a, b) {
+			if (a.breakpoint === b.breakpoint) {
+				if (a.place === b.place) {
+					return 0;
+				}
+
+				if (a.place === "first" || b.place === "last") {
+					return 1;
+				}
+
+				if (a.place === "last" || b.place === "first") {
+					return -1;
+				}
+
+				return b.place - a.place;
+			}
+
+			return b.breakpoint - a.breakpoint;
+		});
+		return;
+	}
+};
+
+const da = new DynamicAdapt("max");
+da.init();;
+let forms = document.querySelectorAll('form');
+if (forms.length > 0) {
+    for (let index = 0; index < forms.length; index++) {
+        const el = forms[index];
+        el.addEventListener('submit', form_submit);
+    }
+}
+
+function form_submit(e) {
+    let btn = event.target;
+    let form = btn.closest('form');
+    let message = form.getAttribute('data-message');
+    let error = form_validate(form);
+    if (error == 0) {
+        form_clean(form);
+        if (message) {
+            /*popup_open('message - ' + message);*/
+            //e.preventDefault();
+        }
+    } else {
+        let form_error = form.querySelectorAll('._error');
+        if (form_error && form.classList.contains('_goto-error')) {
+            _goto(form_error[0], 1000, 50);
+        }
+        event.preventDefault();
+    }
+}
+
+function form_validate(form) {
+    let error = 0;
+    let form_req = form.querySelectorAll('._req');
+    if (form_req.length > 0) {
+        for (let index = 0; index < form_req.length; index++) {
+            const el = form_req[index];
+            if (!_is_hidden(el)) {
+                error += form_validate_input(el);
+            }
+        }
+    }
+    return error;
+}
+
+function _is_hidden(el) {
+    el.hidden;
+}
+
+function form_validate_input(input) {
+    let error = 0;
+    let input_g_value = input.getAttribute('data-value');
+    if (input.getAttribute("name") == "email" || input.classList.contains("_email")) {
+        if (input.value != input_g_value) {
+            let em = input.value.replace(" ", "");
+            input.value = em;
+        }
+        if (!email_test(input.value) || input.value == input_g_value) {
+            form_add_error(input);
+            error++;
+        } else {
+            form_remove_error(input);
+        }
+    } else if (input.getAttribute("type") == "checkbox" && input.checked == false) {
+        form_add_error(input);
+        error++;
+    } else {
+        if (input.value == '' || input.value == input_g_value) {
+            form_add_error(input);
+            error++;
+        } else {
+            form_remove_error(input);
+        }
+    }
+    return error;
+}
+
+function email_test(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function form_add_error(input) {
+    input.classList.add('_error');
+    input.parentElement.classList.add('_error');
+    let input_error = input.parentElement.querySelector('.form__error');
+    if (input_error) {
+        input.parentElement.removeChild(input_error);
+    }
+    let input_error_text = input.getAttribute('data-error');
+    if (input_error_text && input_error_text != '') {
+        input.parentElement.insertAdjacentHTML('beforeend', '<div class="form__error">' + input_error_text + '</div>');
+    }
+}
+
+function form_remove_error(input) {
+    input.classList.remove('_error');
+    input.parentElement.classList.remove('_error');
+    let input_error = input.parentElement.querySelector('.form__error');
+    if (input_error) {
+        input.parentElement.removeChild(input_error);
+    }
+}
+
+function form_clean(form) {
+    let inputs = form.querySelectorAll('input,textarea');
+    for (let index = 0; index < inputs.length; index++) {
+        const el = inputs[index];
+        el.parentElement.classList.remove('_focus');
+        el.classList.remove('_focus');
+        el.value = el.getAttribute('data-value');
+    }
+    let checkboxes = form.querySelectorAll('.checkbox__input');
+    if (checkboxes.length > 0) {
+        for (let index = 0; index < checkboxes.length; index++) {
+            const checkbox = checkboxes[index];
+            checkbox.checked = false;
+        }
+    }
+    let selects = form.querySelectorAll('select');
+    if (selects.length > 0) {
+        for (let index = 0; index < selects.length; index++) {
+            const select = selects[index];
+            const select_default_valut = select.getAttribute('data-default');
+            select.value = select_default_valut;
+            select_item(select);
+        }
+    }
+}
+
+let inputs = document.querySelectorAll('input[data-value],textarea[data-value]');
+inputs_init(inputs);
+
+function inputs_init(inputs) {
+    if (inputs.length > 0) {
+        for (let index = 0; index < inputs.length; index++) {
+            const input = inputs[index];
+            const input_g_value = input.getAttribute('data-value');
+            input_placeholder_add(input);
+            input.addEventListener('focus', function (e) {
+                if (input.value == input_g_value) {
+                    input.value = '';
+                }
+
+                if (input.classList.contains('_phone')) {
+                    input.classList.add('_mask');
+                    Inputmask("+380 (99) 9999999", {
+                        clearIncomplete: true,
+                        clearMaskOnLostFocus: true,
+                        onincomplete: function () {
+                            input_clear_mask(input, input_g_value);
+                        }
+                    }).mask(input);
+                }
+
+                form_remove_error(input);
+            });
+
+            input.addEventListener('blur', function (e) {
+                if (input.value == '') {
+                    input.value = input_g_value;
+                }
+            });
+        }
+    }
+}
+
+function input_placeholder_add(input) {
+    const input_g_value = input.getAttribute('data-value');
+    if (input.value == '' && input_g_value != '') {
+        input.value = input_g_value;
+    }
+}
+
+
+
+;
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll)
+    function animOnScroll(params) {
+        //showHeader();
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+
+    }
+
+    function offset(el) { //функция получения координатов элемента относительно верха или лева страницы
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+
+};
